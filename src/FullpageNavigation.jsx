5f4404f0a7aeda9bc588c0f0c5c28bbb635b0670
Mineru98 <rmstjr1030@naver.com>
2020-07-16 07:57:00 +0900

    Update Code

--- src/FullpageNavigation.jsx
+++ src/FullpageNavigation.jsx
@@ -3,9 +3,9 @@
  * @class FullpageNavigation
  */
 // eslint-disable-next-line react/react-in-jsx-scope
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import FullpageContext from './FullpageContext';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
+import FullpageContext from "./FullpageContext";
 
 // TODO: do navigation
 // eslint-disable-next-line react/prefer-stateless-function
@@ -19,24 +19,18 @@ class FullpageNavigation extends PureComponent {
   };
 
   static propTypes = {
-    style: PropTypes.objectOf(PropTypes.oneOfType([
-      PropTypes.number,
-      PropTypes.string,
-      PropTypes.bool,
-    ])),
-    itemStyle: PropTypes.objectOf(PropTypes.oneOfType([
-      PropTypes.number,
-      PropTypes.string,
-      PropTypes.bool,
-    ])),
+    style: PropTypes.objectOf(
+      PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.bool])
+    ),
+    itemStyle: PropTypes.objectOf(
+      PropTypes.oneOfType([PropTypes.number, PropTypes.string, PropTypes.bool])
+    ),
     reverse: PropTypes.bool,
   };
 
   render() {
     const { style, itemStyle, reverse = false } = this.props;
-    const {
-      number, slides, transitionTiming,
-    } = this.context;
+    const { number, slides, transitionTiming } = this.context;
 
     const gotoSlide = (slide) => {
       const { goto } = this.context;
@@ -44,53 +38,55 @@ class FullpageNavigation extends PureComponent {
     };
 
     return (
-      <div style={{
-        position: 'fixed',
-        height: '100vh',
-        zIndex: 100,
-        top: 0,
-        right: 0,
-        listStyleType: 'none',
-        display: 'flex',
-        flexDirection: 'column',
-        flexWrap: 'nowrap',
-        justifyContent: 'center',
-        paddingRight: '1em',
-        ...style,
-      }}
+      <div
+        style={{
+          position: "fixed",
+          height: "100vh",
+          zIndex: 100,
+          top: 0,
+          right: 0,
+          listStyleType: "none",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          justifyContent: "center",
+          paddingRight: "1em",
+          ...style,
+        }}
       >
-        {
-          slides.map((slide, i) => (
-            <div
-              key={i.toString()}
-            >
-              <div
-                style={{
-                  borderRadius: '50%',
-                  height: (number === i) ? 14 : 10,
-                  width: (number === i) ? 14 : 10,
-                  margin: (number === i) ? 3 : 5,
-                  backgroundColor: (reverse) ? 'white' : 'black',
-                  opacity: (number === i) ? 1 : 0.5,
-                  transition: `all ${transitionTiming * 0.5}ms ease-in-out`,
-                  ...itemStyle,
-                }}
-                onClick={() => gotoSlide(slide)}
-                onKeyPress={() => gotoSlide(slide)}
-                role="button"
-                tabIndex="-1"
-                aria-label={`Slide ${i}`}
-              >
-                <span style={{
-                  display: 'none',
-                }}
+        {slides.map((slide, i) => {
+          if (i != 0 || i != slides.length - 1) {
+            return (
+              <div key={i.toString()}>
+                <div
+                  style={{
+                    borderRadius: "50%",
+                    height: number === i ? 14 : 10,
+                    width: number === i ? 14 : 10,
+                    margin: number === i ? 3 : 5,
+                    backgroundColor: reverse ? "white" : "black",
+                    opacity: number === i ? 1 : 0.5,
+                    transition: `all ${transitionTiming * 0.5}ms ease-in-out`,
+                    ...itemStyle,
+                  }}
+                  onClick={() => gotoSlide(slide)}
+                  onKeyPress={() => gotoSlide(slide)}
+                  role="button"
+                  tabIndex="-1"
+                  aria-label={`Slide ${i}`}
                 >
-                  {`slide number ${i}`}
-                </span>
+                  <span
+                    style={{
+                      display: "none",
+                    }}
+                  >
+                    {`slide number ${i}`}
+                  </span>
+                </div>
               </div>
-            </div>
-          ))
-        }
+            );
+          }
+        })}
       </div>
     );
   }
